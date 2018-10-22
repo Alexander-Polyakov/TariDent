9204062dfbb761a2d85d870b1d78ac74ddebd0e6
Alexander <aleksandr_1992_15@mail.ru>
2018-10-22 11:03:23 +0300

    last

--- src/js/vendors.js
+++ src/js/vendors.js
@@ -1,9 +1,5 @@
 // Если в проекте используются встроенные js-плагины , заменить // на //=
 
-
-//= ../../node_modules/jquery-form-validator/form-validator/jquery.form-validator.js
 //= ../../node_modules/owl.carousel/src/js/owl.carousel.js
 //= ../../node_modules/owl.carousel/src/js/owl.navigation.js
-//= ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
-//= ./plugins/isotope.js
 //= ./plugins/input.mask.js
